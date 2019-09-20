7cbe94bb779e55793928292cd6f4ad689fbda158
Utkarsh Agarwal <uagarwal@purdue.edu>
2019-09-20 19:13:36 -0400

    Removing getters setter

--- Backend/src/main/java/tech/mineapp/model/service/UserDTO.java
+++ Backend/src/main/java/tech/mineapp/model/service/UserDTO.java
@@ -21,46 +21,4 @@ public class UserDTO {
 	private List<String> categoryPreferences;
 	private int numberOfPreviousSearches;
 	private URL profilePicture;
-	public long getUserId() {
-		return UserId;
-	}
-	public void setUserId(long userId) {
-		UserId = userId;
-	}
-	public String getEmailId() {
-		return emailId;
-	}
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public List<String> getCategoryPreferences() {
-		return categoryPreferences;
-	}
-	public void setCategoryPreferences(List<String> categoryPreferences) {
-		this.categoryPreferences = categoryPreferences;
-	}
-	public int getNumberOfPreviousSearches() {
-		return numberOfPreviousSearches;
-	}
-	public void setNumberOfPreviousSearches(int numberOfPreviousSearches) {
-		this.numberOfPreviousSearches = numberOfPreviousSearches;
-	}
-	public URL getProfilePicture() {
-		return profilePicture;
-	}
-	public void setProfilePicture(URL profilePicture) {
-		this.profilePicture = profilePicture;
-	}
 }
