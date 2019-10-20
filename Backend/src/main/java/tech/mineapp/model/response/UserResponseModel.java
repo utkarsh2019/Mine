ed0dae8a2144412ab4b451e681dd0b0ea5e2686a
Utkarsh Agarwal <uagarwal@purdue.edu>
2019-10-20 11:23:26 -0400

    Adding provider to User Response

--- Backend/src/main/java/tech/mineapp/model/response/UserResponseModel.java
+++ Backend/src/main/java/tech/mineapp/model/response/UserResponseModel.java
@@ -1,6 +1,7 @@
 package tech.mineapp.model.response;
 
 import lombok.Data;
+import tech.mineapp.constants.AuthProvider;
 
 /**
  * Response Model representing User Details
@@ -11,8 +12,8 @@ import lombok.Data;
 public class UserResponseModel implements ResponseModel {
 	private String email;
 	private String name;
-	private Boolean isVerified;
 	private String categoryPreferences;
 	private int noOfPreviousSearches;
 	private String profilePicURL;
+	private AuthProvider provider;
 }
