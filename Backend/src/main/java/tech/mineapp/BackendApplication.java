4c9191f41b409ad4014f04aeec491fa04cfe014b
Utkarsh Agarwal <uagarwal@purdue.edu>
2019-10-30 20:02:20 -0400

    Profile pic uploading implemented

--- Backend/src/main/java/tech/mineapp/BackendApplication.java
+++ Backend/src/main/java/tech/mineapp/BackendApplication.java
@@ -1,14 +1,14 @@
 package tech.mineapp;
 
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 
 import tech.mineapp.config.AppPropertiesConfig;
 import tech.mineapp.config.StorageProperties;
-import tech.mineapp.service.FileUploadService;
+//import tech.mineapp.service.FileUploadService;
 
 @SpringBootApplication
 @EnableConfigurationProperties({AppPropertiesConfig.class, StorageProperties.class})
@@ -17,10 +17,10 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 	
-	@Bean
-	CommandLineRunner init(FileUploadService fileUploadService) {
-		return (args) -> {
-			fileUploadService.init();
-		};
-	}
+//	@Bean
+//	CommandLineRunner init(FileUploadService fileUploadService) {
+//		return (args) -> {
+//			fileUploadService.init();
+//		};
+//	}
 }
--- Backend/src/main/java/tech/mineapp/service/FileUploadService.java
+++ Backend/src/main/java/tech/mineapp/service/FileUploadService.java
@@ -1,22 +1,13 @@
 package tech.mineapp.service;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import tech.mineapp.config.StorageProperties;
-import tech.mineapp.exception.StorageException;
-import tech.mineapp.exception.StorageFileNotFoundException;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 
 /**
  * @author utkarsh
@@ -25,68 +16,14 @@ import tech.mineapp.exception.StorageFileNotFoundException;
 @Service
 public class FileUploadService {
 	
-	private final Path rootLocation;
+	private static final Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+			"cloud_name", "mineapp",
+			"api_key", "934122198185593",
+			"api_secret", "t-cXLtSFEPqROk8IechyX8nGCQw"));
 	
-	@Autowired
-	public FileUploadService(StorageProperties properties) {
-		this.rootLocation = Paths.get(properties.getLocation());
-	}
-	
-	public void store(MultipartFile file, String filename) {
-		
-		try {
-			if (file.isEmpty()) {
-				throw new StorageException("Failed to store empty file");
-			}
-//			if (filename.contains("..")) {
-//				throw new StorageException("Cannot store file with relative path outside current directory "+filename);
-//			}
-			try (InputStream inputStream = file.getInputStream()) {
-				Files.copy(inputStream, load(filename), StandardCopyOption.REPLACE_EXISTING);
-			}
-		} catch (IOException e) {
-			throw new StorageException("Failed to store file "+filename, e);
-		}
-	}
-	
-	public void delete(String filename) {
-		try {
-			Path file = load(filename);
-			if (Files.notExists(file)) {
-				throw new StorageFileNotFoundException("Could not find file.");
-			} else {
-				Files.delete(file);
-			}
-		} catch (IOException e) {
-			throw new StorageFileNotFoundException("Could not read file.", e);
-		}
-	}
-	
-	public Path load(String filename) {
-		return rootLocation.resolve(filename);
-	}
-	
-	public Resource loadAsResource(String filename) {
-		try {
-			Path file = load(filename);
-			Resource resource = new UrlResource(file.toUri());
-			if (resource.exists() || resource.isReadable()) {
-				return resource;
-			} else {
-				throw new StorageFileNotFoundException("Could not find file.");
-			}
-		} catch (MalformedURLException e) {
-			throw new StorageFileNotFoundException("Could not read file.", e);
-		}
-	}
-	
-	public void init() {
-		try {
-			if (Files.notExists(rootLocation)) {
-				Files.createDirectories(rootLocation);
-			}
-		} catch (IOException e) {
-			throw new StorageException("Could not initialize storage", e);
-		}
+	@SuppressWarnings("rawtypes")
+	public String uploadPhoto(MultipartFile file) throws IOException {
+		Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap("resource_type", "image"));
+		return (String) uploadResult.get("url");
 	}
 }
