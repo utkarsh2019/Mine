7aeb8b5a4752b166a88a69dcf49b959c74637093
Amol Moses Jha <amolmoses@gmail.com>
2019-11-01 05:36:42 -0400

    Unit tests and HTML parsing

--- Frontend/src/constants/index.js
+++ Frontend/src/constants/index.js
@@ -1,7 +1,11 @@
-export const API_BASE_URL = 'http://api.mineapp.tech';
-export const ACCESS_TOKEN = 'accessToken';
+export const API_BASE_URL = "http://localhost:8080";
+export const ACCESS_TOKEN = "accessToken";
 
-export const OAUTH2_REDIRECT_URI = 'http://mineapp.tech/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = "http://mineapp.tech/oauth2/redirect";
 
-export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const GOOGLE_AUTH_URL =
+  API_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI;
+export const FACEBOOK_AUTH_URL =
+  API_BASE_URL +
+  "/oauth2/authorize/facebook?redirect_uri=" +
+  OAUTH2_REDIRECT_URI;
