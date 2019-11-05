b73d00755f738b3dc9231361d3765c38d095ab10
chands1997 <chands@purdue.edu>
2019-11-05 18:41:53 -0500

    setup for https

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -7,11 +7,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <App />,
     document.getElementById("root")
-
 );
 
 registerServiceWorker();
