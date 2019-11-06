15525d6330c67fa9b6def25f2e2d14ba01b57e47
Utkarsh Agarwal <uagarwal@purdue.edu>
2019-11-05 21:04:19 -0500

    Localhost working with webpack

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -1,66 +1,38 @@
-const path = require('path');
-const webpack = require('webpack');
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
-
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
 module.exports = {
-	mode: 'development',
+	context: __dirname,
 	entry: './src/index.js',
-
 	output: {
+		path: path.resolve( __dirname, 'dist' ),
 		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/'
+		publicPath: '/',
 	},
-
-	plugins: [
-		new webpack.ProgressPlugin(),
-		new HtmlWebpackPlugin()
-	],
-
+	devServer: {
+		inline: true,
+		port: 3000,
+		historyApiFallback: true
+	},
 	module: {
 		rules: [
 			{
-            			test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-            			use: 'babel-loader',
-         		},
-         		{
-            			test: /\.css$/,
-            			use: ['style-loader', 'css-loader'],
-         		},
-         		{
-            			test: /\.(png|jpg|svg|gif)?$/,
-            			use: 'file-loader'
-         		}
+				test: /\.js$/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|jpg|svg|gif)?$/,
+				use: 'file-loader'
+			}
 		]
 	},
-
-	devServer: {
-		inline: true,
-		host: '0.0.0.0',
-		disableHostCheck: true
-	}
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: path.resolve( __dirname, 'public/index.html' ),
+			filename: 'index.html'
+		})
+	]
 };
